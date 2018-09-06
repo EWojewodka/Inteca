410e8e1b114477282c9c73c057cc43ad97967c92
ewojew <emilwojewodka@gmail.com>
2018-09-06 17:57:39 +0200

    add webapp to docker compse

--- src/main/java/com/wojewodka/inteca/IntecaApplication.java
+++ src/main/java/com/wojewodka/inteca/IntecaApplication.java
@@ -2,11 +2,29 @@ package com.wojewodka.inteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
+@ComponentScan
 public class IntecaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntecaApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
 }
--- src/main/java/com/wojewodka/inteca/api/FamilyController.java
+++ src/main/java/com/wojewodka/inteca/api/FamilyController.java
@@ -58,4 +58,5 @@ public class FamilyController {
 
 		return result;
 	}
+
 }
